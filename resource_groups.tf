f4ee519bbc433f973a449744c519953a84976c68
Filip Dabrowski <filip.a.dabrowski@gmail.com>
2026-02-20 16:07:20 +0100

    fix comments

--- resource_groups.tf
+++ resource_groups.tf
@@ -4,14 +4,14 @@ locals {
   }
 }
 
-# resource group from hub
+# resource group for hub
 resource "azurerm_resource_group" "rg-prod-pl-hub" {
   name = "rg-prod-pl-hub"
   location = var.location
   tags = local.rg_tags
 }
 
-# resource group from spoke
+# resource group for spoke
 resource "azurerm_resource_group" "rg-prod-pl-hub" {
   name = "rg-prod-pl-hub"
   location = var.location
