8738a504844ff02747bed320478775ceb4e485d9
Filip Dabrowski <filip.a.dabrowski@gmail.com>
2026-02-21 14:51:23 +0100

    update resource groups - simplified names

--- resource_groups.tf
+++ resource_groups.tf
@@ -1,19 +1,13 @@
-locals {
-  rg_tags = {
-    "Environment" = "Prod"
-  }
-}
-
 # resource group for hub
-resource "azurerm_resource_group" "rg-prod-pl-hub" {
+resource "azurerm_resource_group" "hub" {
   name = "rg-prod-pl-hub"
   location = var.location
-  tags = local.rg_tags
+  tags = local.shared_tags
 }
 
 # resource group for spoke
-resource "azurerm_resource_group" "rg-prod-pl-hub" {
-  name = "rg-prod-pl-hub"
+resource "azurerm_resource_group" "spoke" {
+  name = "rg-prod-pl-spoke"
   location = var.location
-  tags = local.rg_tags
+  tags = local.shared_tags
 }
