1936a6b486c07f77c62b5508d857971fcf7f7efc
Filip Dabrowski <filip.a.dabrowski@gmail.com>
2026-02-21 16:47:00 +0100

    add network module

---
+++ modules/network/main.tf
@@ -0,0 +1,16 @@
+# the module will create vnets and subnets defined in vnets.tf in the root directory
+resource "azurerm_virtual_network" "default" {
+  name                = var.name
+  resource_group_name = var.resource_group_name
+  location            = var.location
+  address_space       = var.address_space
+  tags                = var.tags
+}
+
+resource "azurerm_subnet" "default" {
+  for_each             = var.subnets
+  name                 = each.key
+  resource_group_name  = azurerm_virtual_network.default.resource_group_name
+  virtual_network_name = azurerm_virtual_network.default.name
+  address_prefixes     = [each.value]
+}
---
+++ modules/network/variables.tf
@@ -0,0 +1,24 @@
+variable "name" {
+  type = string
+}
+
+variable "resource_group_name" {
+  type = string
+}
+
+variable "location" {
+  type = string
+}
+
+variable "address_space" {
+  type = list(string)
+}
+
+variable "tags" {
+  type = map(string)
+}
+
+variable "subnets" {
+  type    = map(string)
+  default = {}
+}
