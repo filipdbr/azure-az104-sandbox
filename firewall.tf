ca85925c1f7b4e518dac88e20234fc35448a28aa
Filip Dabrowski <filip.a.dabrowski@gmail.com>
2026-02-24 18:31:59 +0100

    moved routes to a dedicated file
    also assigned the table to web subnet

--- firewall.tf
+++ firewall.tf
@@ -24,32 +24,3 @@ resource "azurerm_firewall" "default" {
 
   tags = local.shared_tags
 }
-
-# create a route table for spoke
-resource "azurerm_route_table" "spoke" {
-  name                = "rt-prod-pl-spoke"
-  resource_group_name = azurerm_resource_group.spoke.name
-  location            = azurerm_resource_group.spoke.location
-  tags                = local.shared_tags
-}
-
-# create a rule for the route table: all outbound traffic to the internet is to go throught the firewall
-resource "azurerm_route" "to_firewall" {
-  name                   = "route-to-fw-prod-pl"
-  resource_group_name    = azurerm_resource_group.spoke.name
-  route_table_name       = azurerm_route_table.spoke.name
-  address_prefix         = "0.0.0.0/0"
-  next_hop_type          = "VirtualAppliance"
-  next_hop_in_ip_address = azurerm_firewall.default.ip_configuration[0].private_ip_address
-}
-
-# associate the table with web and db subnets
-resource "azurerm_subnet_route_table_association" "web" {
-  subnet_id = module.spoke_vnet.subnets["snet-prod-pl-web"].id
-  route_table_id = azurerm_route_table.spoke.id
-}
-
-resource "azurerm_subnet_route_table_association" "db" {
-  subnet_id = module.spoke_vnet.subnets["snet-prod-pl-db"].id
-  route_table_id = azurerm_route_table.spoke.id
-}
