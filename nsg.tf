cbfb296a1dab837457f6fedcdc8196728e6ddb22
Filip Dabrowski <filip.a.dabrowski@gmail.com>
2026-02-24 16:50:17 +0100

    add nsg for web tier / subnet

--- nsg.tf
+++ nsg.tf
@@ -1,6 +1,7 @@
+# NSG for app gateway subnet
 resource "azurerm_network_security_group" "agg_gateway" {
-  name = "nsg-prod-pl-spoke-appgw"
-  location = azurerm_resource_group.spoke.location
+  name                = "nsg-prod-pl-spoke-appgw"
+  location            = azurerm_resource_group.spoke.location
   resource_group_name = azurerm_resource_group.spoke.name
 
   security_rule {
@@ -33,4 +34,28 @@ resource "azurerm_network_security_group" "agg_gateway" {
 resource "azurerm_subnet_network_security_group_association" "app_gateway" {
   subnet_id                 = module.spoke_vnet.subnets["snet-prod-pl-appgw"].id
   network_security_group_id = azurerm_network_security_group.agg_gateway.id
+}
+
+# NSG for web subnet in spoke vnet
+resource "azurerm_network_security_group" "web" {
+  name                = "nsg-prod-pl-spoke-web"
+  location            = azurerm_resource_group.spoke.location
+  resource_group_name = azurerm_resource_group.spoke.name
+
+  security_rule {
+    name                       = "AllowHTTPInbound"
+    priority                   = 200
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "80"
+    source_address_prefix      = module.spoke_vnet.subnets["snet-prod-pl-appgw"].address_prefixes[0]
+    destination_address_prefix = "*"
+  }
+}
+
+resource "azurerm_subnet_network_security_group_association" "web" {
+  subnet_id                 = module.spoke_vnet.subnets["snet-prod-pl-web"].id
+  network_security_group_id = azurerm_network_security_group.web.id
 }
