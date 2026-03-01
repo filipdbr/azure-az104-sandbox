ff15e87b9e05930da560e7f7f44fba30efd9e0e8
Filip Dabrowski <filip.a.dabrowski@gmail.com>
2026-03-01 13:58:54 +0100

    add outputs needed for NIC backend pool association (for internal lb)

--- modules/compute/app_servers/output.tf
+++ modules/compute/app_servers/output.tf
@@ -4,3 +4,13 @@ output "app_vm_private_ips" {
   }
   description = "map of private IPs associated with app servers"
 }
+
+output "app_nic_ids" {
+  value = azurerm_network_interface.app[*].id
+  description = "List of NICs for VMs in app tier"
+}
+
+output "app_ip_config_names" {
+  value = azurerm_network_interface.app[*].ip_configuration[0].name
+}
+
