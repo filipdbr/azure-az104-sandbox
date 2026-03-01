d9301f58cf811e91c088634d388ab7ce123f142a
Filip Dabrowski <filip.a.dabrowski@gmail.com>
2026-03-01 14:26:18 +0100

    feat(network): implemented association of app-tier VMs NICs to the backend pool
    - all NICs associated with app tier VMs will be associated with the backend pool of an internal LB
    - dynamic assignement

--- loadbalancing.tf
+++ loadbalancing.tf
@@ -18,3 +18,12 @@ module "internal_lb_app" {
   location            = azurerm_resource_group.spoke.location
   subnet_id           = module.spoke_vnet.subnets["snet-prod-pl-app"].id
 }
+
+# associate VMs with backend pool for and app internal load balancer
+resource "azurerm_network_interface_backend_address_pool_association" "app_tier" {
+  # use the output of /modules/compute/app_servers - number of VMs
+  count = length(module.app_servers.app_nic_ids)
+  network_interface_id = module.app_servers.app_nic_ids[count.index]
+  ip_configuration_name = module.app_servers.app_ip_config_names[count.index]
+  backend_address_pool_id = module.internal_lb_app.backend_address_pool_id
+}
