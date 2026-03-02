00413b760795344ea59fe65751f63361266d1538
Filip Dabrowski <filip.a.dabrowski@gmail.com>
2026-03-02 16:48:31 +0100

    add variables for db module

---
+++ modules/database/variables.tf
@@ -0,0 +1,38 @@
+variable "server_name" {
+  type = string
+}
+
+variable "db_name" {
+  type = string
+}
+
+variable "resource_group_name" {
+  type = string
+}
+
+variable "location" {
+  type = string
+}
+
+variable "admin_login" {
+  type = string
+}
+
+variable "admin_pwd" {
+  type = string
+}
+
+variable "collation" {
+  type = string
+  default = "SQL_Latin1_General_CP1_CI_AS"
+}
+
+variable "prevent_destroy" {
+  type = bool
+  default = true
+}
+
+variable "tags" {
+  type = map(string)
+  default = { }
+}
